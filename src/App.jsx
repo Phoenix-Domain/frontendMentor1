11959ddc0a8057611ed0183ad5b23280d90cf385
Batubo Victory <victorybatubo76@gmail.com>
2025-08-27 15:29:14 +0100

    Edit: import Card component and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-
+import Card from "./components/Card"
 
 function App() {
 
   return (
     <main className="bg-[rgb(242,222,106)] w-[100dvw] h-[100dvh]">
-      
+      <Card />
     </main>
   )
 }
