ecb95e5b10c98ce9bc7b50ac51c39ce8b0b83631
Batubo Victory <victorybatubo76@gmail.com>
2025-08-27 15:59:08 +0100

    Style: Add black shadow and make div rounder

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import Image from "./Image";
 
 function Card(){
   return(
-    <div className="bg-white w-fit h-fit p-3 rounded-xl m-auto">
+    <div className="bg-white w-fit h-fit p-5 rounded-2xl m-auto border-1 shadow-[7px_7px_0px_#000]">
       <Image imgSrc='src\assets\images\illustration-article.svg' style='rounded-xl'/>
 
     <Button />
