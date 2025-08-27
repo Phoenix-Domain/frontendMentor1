59dc868884dd178e585072cc3e46876c6b2c33b8
Batubo Victory <victorybatubo76@gmail.com>
2025-08-27 15:41:20 +0100

    Style: round the div and image borders

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 function Card(){
   return(
-    <div className="bg-white w-fit h-fit p-1 m-auto">
-      <img src="src\assets\images\illustration-article.svg" alt="" />
+    <div className="bg-white w-fit h-fit p-3 rounded-xl m-auto">
+      <img src="src\assets\images\illustration-article.svg" className="rounded-xl" alt="" />
 
     </div>
   )
