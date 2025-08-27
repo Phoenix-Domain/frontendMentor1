2d2b279540352c63d3440814417c1ce2825a6a66
Batubo Victory <victorybatubo76@gmail.com>
2025-08-27 15:59:44 +0100

    Style: Add style to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button(){
   return(
-    <button className="bg-[rgb(244,208,78)] rounded-md px-3 py-1 font-bold">
+    <button className="bg-[rgb(244,208,78)] rounded-md px-3 py-1 font-bold my-5">
       Learning
     </button>
   )
