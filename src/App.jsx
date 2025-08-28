48252ec06dd116e26060a3cd958919759e1d4ea6
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 13:50:23 +0100

    Style: change background color and center card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from "./components/Card"
 function App() {
 
   return (
-    <main className="">
+    <main className="bg-[hsl(0,0%,8%)] w-[100dvw] h-[100dvh] flex items-center justify-center">
       <Card />
     </main>
   )
