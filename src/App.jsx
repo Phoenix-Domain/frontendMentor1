f697954a7bbb2621daaefacf100cc79b2ddaef64
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 13:29:43 +0100

    Edit: delete previous styles and remove card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Card from "./components/Card"
+
 
 function App() {
 
   return (
-    <main className="bg-[rgb(244,208,78)] w-[100dvw] h-[100dvh] flex items-center justify-center ">
-      <Card />
+    <main className="">
+      
     </main>
   )
 }
