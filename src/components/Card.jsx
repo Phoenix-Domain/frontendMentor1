baf1c7df0304be64cd3976d639f34b76af40a851
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 11:57:46 +0100

    Edit: set card max width at 375px to ensure responsiveness and render paragraph component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import Header from "./Header";
 
 function Card(){
   return(
-    <div className="bg-white w-fit h-fit p-5 rounded-2xl m-auto border-1 shadow-[7px_7px_0px_#000]">
+    <div className="bg-white max-w-[375px] h-fit p-5 rounded-2xl m-auto border-1 shadow-[7px_7px_0px_#000]">
       <Image imgSrc='src\assets\images\illustration-article.svg' style='rounded-xl'/>
 
     <Button />
@@ -13,6 +13,9 @@ function Card(){
     <Paragraph content='Published 21 Dec 2023' style='font-semibold' />
 
     <Header size='1' content='HTML & CSS foundations' style='text-2xl font-bold my-4' />
+
+    <Paragraph content='These languages are the backbone of every website, defining structure, content and presentation' style='text-gray-500' />
+
     </div>
   )
 }
