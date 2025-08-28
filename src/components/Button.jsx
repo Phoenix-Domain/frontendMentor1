97f0f66d634cd3df85717bc991ace1735624f1d0
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 14:30:27 +0100

    Finish next challenge

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button ({ content }){
+  return (
+    <button className='w-full bg-[hsl(0,0%,20%)] text-white py-2 rounded-md font-bold'>
+      {content}
+    </button>
+  )
+}
+
+export default Button
---
+++ src/components/Text.jsx
@@ -0,0 +1,9 @@
+function Text({ style, content }){
+  return(
+    <p className={style}>
+      {content}
+    </p>
+  )
+}
+
+export default Text
