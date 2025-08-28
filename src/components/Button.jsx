6db46b52e5fe3da56e92a6496eebd649ef9b076f
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 14:44:05 +0100

    add hover state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button ({ content }){
   return (
-    <button type='button' className='w-full bg-[hsl(0,0%,20%)] text-white py-2 rounded-md font-bold'>
+    <button type='button' className='w-full bg-[hsl(0,0%,20%)] text-white py-2 rounded-md font-bold hover:bg-[hsl(75,94%,57%)]'>
       {content}
     </button>
   )
