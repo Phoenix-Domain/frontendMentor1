6f131a3874d25d50852eff8658f93cd9e5081cd2
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 12:05:01 +0100

    Edit: center card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from "./components/Card"
 function App() {
 
   return (
-    <main className="bg-[rgb(244,208,78)] w-[100dvw] h-[100dvh]">
+    <main className="bg-[rgb(244,208,78)] w-[100dvw] h-[100dvh] flex items-center justify-center ">
       <Card />
     </main>
   )
