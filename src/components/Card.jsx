755fa78f7fd0e5e4df1e1dadf7247a9e0966bee4
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 11:59:54 +0100

    Edit: add image component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,13 +8,17 @@ function Card(){
     <div className="bg-white max-w-[375px] h-fit p-5 rounded-2xl m-auto border-1 shadow-[7px_7px_0px_#000]">
       <Image imgSrc='src\assets\images\illustration-article.svg' style='rounded-xl'/>
 
-    <Button />
+      <Button />
 
-    <Paragraph content='Published 21 Dec 2023' style='font-semibold' />
+      <Paragraph content='Published 21 Dec 2023' style='font-semibold' />
 
-    <Header size='1' content='HTML & CSS foundations' style='text-2xl font-bold my-4' />
+      <Header size='1' content='HTML & CSS foundations' style='text-2xl font-bold my-4' />
 
-    <Paragraph content='These languages are the backbone of every website, defining structure, content and presentation' style='text-gray-500' />
+      <Paragraph content='These languages are the backbone of every website, defining structure, content and presentation' style='text-gray-500' />
+
+     <div>
+      <Image imgSrc='src\assets\images\image-avatar.webp' style='w-10'/>
+     </div>
 
     </div>
   )
