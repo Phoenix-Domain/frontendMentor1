deb1074b0cc1c4240ae675d26aba68f78698bdd0
Batubo Victory <victorybatubo76@gmail.com>
2025-08-28 12:30:37 +0100

    Fix: make all image paths relative to ensure proper build

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ import Header from "./Header";
 function Card(){
   return(
     <div className="bg-white max-w-[375px] h-fit p-5 rounded-2xl m-auto border-1 shadow-[7px_7px_0px_#000]">
-      <Image imgSrc='src\assets\images\illustration-article.svg' style='rounded-xl' altText='Card Design'/>
+      <Image imgSrc='.\src\assets\images\illustration-article.svg' style='rounded-xl' altText='Card Design'/>
 
       <Button />
 
@@ -17,7 +17,7 @@ function Card(){
       <Paragraph content='These languages are the backbone of every website, defining structure, content and presentation' style='text-gray-500' />
 
      <div className="mt-4 flex gap-4 items-center w-fit">
-      <Image imgSrc='src\assets\images\image-avatar.webp' style='w-10'/>
+      <Image imgSrc='.\src\assets\images\image-avatar.webp' style='w-10'/>
 
       <Paragraph content='Greg Hooper' style='font-bold' altText='Greg Hooper'/>
      </div>
